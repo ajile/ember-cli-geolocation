517d99d1569e0421b6de0948ad60a1ce60e15765
ajile <v.v.milkov@gmail.com>
2014-11-03 19:59:39 +0300

    Quick fix

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,8 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
-app.import('bower_components/geoPosition/js/geoPosition.js');
+// app.import('bower_components/geoPosition/js/geoPosition.js');
+app.import('vendor/geoPosition.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
