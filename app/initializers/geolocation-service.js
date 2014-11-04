5a8d405e442797610eed84eabcfb5dea5230ebd6
ajile <v.v.milkov@gmail.com>
2014-11-04 15:02:11 +0300

    Added geolocation service into view namespace

--- app/initializers/geolocation-service.js
+++ app/initializers/geolocation-service.js
@@ -1,6 +1,7 @@
 export function initialize(container, application) {
   application.inject('route', 'geolocationService', 'service:geolocation');
   application.inject('controller', 'geolocation', 'service:geolocation');
+  application.inject('view', 'geolocation', 'service:geolocation');
 };
 
 export default {
