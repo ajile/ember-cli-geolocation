1f035d02e8c4f426da2963b68a61662cf2c81c35
ajile <v.v.milkov@gmail.com>
2014-11-05 19:06:43 +0300

    Tabs on space
    Make prop readOnly
    
    Change assert condition
    
    Tests for mixin

--- app/initializers/geolocation-service.js
+++ app/initializers/geolocation-service.js
@@ -1,10 +1,10 @@
 export function initialize(container, application) {
-  application.inject('route', 'geolocationService', 'service:geolocation');
-  application.inject('controller', 'geolocation', 'service:geolocation');
-  application.inject('view', 'geolocation', 'service:geolocation');
+    application.inject('route', 'geolocation', 'service:geolocation');
+    application.inject('controller', 'geolocation', 'service:geolocation');
+    application.inject('view', 'geolocation', 'service:geolocation');
 };
 
 export default {
-  name: 'geolocation-service',
-  initialize: initialize
+    name: 'geolocation-service',
+    initialize: initialize
 };
